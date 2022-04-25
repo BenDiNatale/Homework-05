a7cd34b02af573cdee617b5088a48ccc2ba91354
Ben DiNatale96 <bend6991@gmail.com>
2022-04-25 13:44:22 -0400

    rearranged how I wanted to approach. Will use a jscript variable to loop through and save information pertaining to the day, minimal html and css to be used.

---
+++ assets/script.js
@@ -0,0 +1,74 @@
+const myDay = [
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+{
+    id: "0",
+    hour: "09",
+    time:"09",
+    abbreviation: "AM",
+    reminder: "",
+
+},
+]
